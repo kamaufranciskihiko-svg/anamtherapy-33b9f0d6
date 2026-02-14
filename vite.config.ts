7a04c30b233d2c12aba7a4c0742cb9a82a2da1ed
kamaufranciskihiko-svg <kamau.francis.kihiko@gmail.com>
2026-02-14 06:28:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/anamtherapy-33b9f0d6/", // <-- required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
