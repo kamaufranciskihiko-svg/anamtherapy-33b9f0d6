2f064c36eb08d533f06c1ab3f2cce73c793ea5a7
kamaufranciskihiko-svg <kamau.francis.kihiko@gmail.com>
2026-02-14 12:30:42 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // use HashRouter for GH Pages
-import Index from "./index";
-import "./index.css"; // Tailwind/global styles
+import { HashRouter } from "react-router-dom";
+import App from "./App";  // ← Changed from "./index"
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
-      <Index />
+      <App />  {/* ← Changed from <Index /> */}
     </HashRouter>
   </React.StrictMode>
 );
