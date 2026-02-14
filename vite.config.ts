629e933c2699f052d04f873757bb8799ac24aea7
kamaufranciskihiko-svg <kamau.francis.kihiko@gmail.com>
2026-02-14 07:03:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/anamtherapy-33b9f0d6/",    // ⚠ THIS IS CRUCIAL
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: "/anamtherapy-33b9f0d6/",  // <<--- This tells Vite to prefix all assets with the repo name
   server: {
     host: "::",
     port: 8080,
     hmr: { overlay: false },
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
 }));
